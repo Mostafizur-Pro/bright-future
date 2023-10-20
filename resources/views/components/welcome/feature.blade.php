ccb2ce3fb417cf4e666912082d74f5f24a17a1d9
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-10-20 09:32:00 +0600

    course feature add

---
+++ resources/views/components/welcome/feature.blade.php
@@ -0,0 +1,51 @@
+<div class="relative">
+        <div class="flex justify-center">
+            <div class="flex justify-center mx-auto gap-10">
+                <!-- Card 1 -->
+                <div class="c1 card card-compact w-96 bg-base-100 hover:bg-accent hover:text-white  -top-8 hover:-top-12 shadow-xl pt-20">
+                    <figure><img class="w-20" src="{{url('assets/home/feature/programming.png')}}" alt="image" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Programming Courses</h2>
+                        <p>Our Delmont hospital utilizes state of that art technology and employs a team of true experts</p>
+                        <div class="card-actions justify-end">
+                            <!-- <button class="btn btn-primary">Buy Now</button> -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Card 2 -->
+                <div class="card card-compact w-96 bg-base-100 hover:bg-accent hover:text-white  -top-8 hover:-top-12 shadow-xl pt-20">
+                    <figure><img class="w-20" src="{{url('assets/home/feature/graphic.png')}}" alt="image" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Graphic Design Courses</h2>
+                        <p>Our Delmont hospital utilizes state of that art technology and employs a team of true experts</p>
+                        <div class="card-actions justify-end">
+                            <!-- <button class="btn btn-primary">Buy Now</button> -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Card 3 -->
+                <div class="card card-compact w-96 bg-base-100 hover:bg-accent hover:text-white  -top-8 hover:-top-12 shadow-xl pt-20">
+                    <figure><img class="w-20" src="{{url('assets/home/feature/autocad.png')}}" alt="image" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">AutoCAD Course</h2>
+                        <p>Our Delmont hospital utilizes state of that art technology and employs a team of true experts</p>
+                        <div class="card-actions justify-end">
+                            <!-- <button class="btn btn-primary">Buy Now</button> -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Card 4 -->
+                <div class="card card-compact w-96 bg-base-100 hover:bg-accent hover:text-white  -top-8 hover:-top-12 shadow-xl pt-20">
+                    <figure><img class="w-20" src="{{url('assets/home/feature/mep.png')}}" alt="image" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">MEP Course</h2>
+                        <p>Our Delmont hospital utilizes state of that art technology and employs a team of true experts</p>
+                        <div class="card-actions justify-end">
+                            <!-- <button class="btn btn-primary">Buy Now</button> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
